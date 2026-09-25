852e92cdf28c863a5a79c849df9528971e9f8a86
KillaKM <kaimolendijk@gmail.com>
2026-09-25 19:59:07 +0200

    Update BasketbalApp.py

--- BasketbalApp.py
+++ BasketbalApp.py
@@ -1674,5 +1674,5 @@ if "sheets" in st.session_state:
 
 else:
     st.warning(
-        "👈 Upload aan de linkerkant je Excel-bestand om te beginnen ('SKP Schedule 26_27 (BOARD).xlsx')."
+        "👈 Upload aan de linkerkant je Excel-bestand om te beginnen"
     )
